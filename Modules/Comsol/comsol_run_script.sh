43fddf1f82b7d19ddc1349004acf869c889c70b9
Jiannong <jnfangch@yahoo.com>
2018-09-07 10:34:50 +0200

    Added comsol examples and notes

---
+++ Modules/Comsol/comsol_run_script.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#SBATCH --nodes 2
+
+#SBATCH --ntasks 4
+
+#SBATCH --cpus-per-task 8
+
+#SBATCH --ntasks-per-node 2
+
+#SBATCH --time 01:00:00
+
+#SBATCH --mem 10240
+
+#SBATCH --output=slurm.%N.%j.out
+#SBATCH --error=slurm.%N.%j.err
+
+module purge
+
+module load comsol
+
+ulimit -s unlimited
+ulimit -c unlimited
+
+echo " ****** START OF JOB ******"
+echo $SLURM_NTASKS
+echo $SLURM_NTASKS_PER_NODE
+echo $SLURM_CPUS_PER_TASK
+
+hostfile=hostlist-job${SLURM_JOB_ID}
+srun hostname | sort > ${hostfile}
+
+comsol batch -clustersimple -nn $SLURM_NTASKS -nnhost $SLURM_NTASKS_PER_NODE -np $SLURM_CPUS_PER_TASK -f ${hostfile} -mpifabrics shm:ofa -inputfile test.mph -outputfile testoutput.mph
+
+echo " ****** END OF JOB ******"
+
+STATUS=$status
+echo ""
+echo "==> Resulting STATUS = $STATUS"
+echo
+
